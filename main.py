3b6a2d124bc38620e62ce9c4c29905859b6f8a1a
renanhiramatsu <renansilvahiramatsu@gmail.com>
2022-04-15 07:50:18 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Hotels(Resource):
+    def get(self):
+        return {
+            'hotels': ['Marriot', 'Hilton', 'Holiday Inn']
+        }
